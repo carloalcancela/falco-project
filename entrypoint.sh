ccd2071d3237d42e9b56b7ee406751493e49a30c
Carlos Lucas <122367934+carloalcancela@users.noreply.github.com>
2023-05-02 16:14:43 -0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Download the file from s3 to the local directory
-aws s3 cp aws s3 cp s3://falco-data-lake/eb_falco.yaml /data/falco.yaml
-aws s3 cp aws s3 cp s3://falco-data-lake/falco_rules.local.yaml /data/falco_rules.local.yaml
